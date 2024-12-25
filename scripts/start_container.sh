b22a7a3ee1a644dfad68de470e9665ff8be10f1c
AjitAdin <ajitadin03@gmail.com>
2024-12-25 15:50:40 +0530

    changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull ajitadin/simple-proj-flask
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 AjitAdin/simple-proj-flask
+docker run -d -p 5000:5000 ajitadin/simple-proj-flask
